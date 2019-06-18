11f9254138da40c3c4d527d428b23af793e9d64a
Hassan Harb <hharb@ucmerced.edu>
2019-06-18 14:12:10 -0500

    Major Fixes/Changes
    1. NIO code now uses BEATLES library
    2. Code outputs NIO/MO populations
    3. Minor bug fixes

--- nio.py
+++ nio.py
@@ -11,7 +11,7 @@
 #
 #  For more details, check: J. Chem. Phys., 144, 204117 (2016)
 #
-#  Last edited by Hassan Harb, June 5, 2019
+#  Last edited by Hassan Harb, June 18, 2019
 #
 #######################################################################################################################
 
@@ -23,7 +23,7 @@ import numpy as np
 from numpy import genfromtxt
 import csv
 from decimal import Decimal 
-
+from BEATLES import *
 
 #Function: Symmetrize: Transform packed array into NBasis x NBasis 
 def symmetrize(a):
@@ -37,26 +37,32 @@ def symmetrize(a):
       n=n+1
   return b
 
-#Function: convert output to scientific notation
+# Function: convert output to scientific notation
 def sci_notation(n):
     a = '%.8E' % n
     return '%.8E' % Decimal(n.real)
 
-#Function: Calculate Ncol, the column vector that shows the MO contributions of NIOs
+# Function: Calculate Ncol, the column vector that shows the MO contributions of NIOs
 def CalNCol(T,k,NBasis,S):
    Ti = np.zeros(NBasis)
    for i in range(0,NBasis):
       Ti[i] = T[i,k].real
 
+   T_in = np.dot(Ti,Ti)
    N = np.outer(Ti,Ti)
+   I = np.identity(NBasis)
+   N  = np.multiply(N,I)
+   N_cont = np.sum(N)
+   print "Inner product =", T_in
+   print "Contraction = ", N_cont
    print "trace N = ", np.trace(N)
-   N = np.multiply(N,S)
-   print "trace N.S = ", np.trace(N), "\n" 
+
    NCol = np.zeros(NBasis)
    for i in range(0,NBasis):
        for j in range(0,NBasis):
           NCol[i] = NCol[i] + N[j,i]
-
+       if (NCol[i] < 0.0000000001 ):
+          NCol[i] = 0
    NIOPops(NCol,NBasis)
 
 #Function: Calculate the percentage contributions of MOs in each NIO
@@ -89,9 +95,7 @@ with open(filename1,'r') as origin:
 
 print "Number of Basis Functions = ", NBasis, "\n"
 MOElements = NBasis * NBasis
-print "The code will look for ", MOElements, " elements of the MO Coeffienct matrix\n"
 PElements = int(NBasis*(NBasis+1)/2)
-print "The code will look for ", PElements, " elements of the Density matrix\n"
 NBasis2=0
 
 with open(filename2,'r') as origin:
@@ -115,9 +119,6 @@ if (MOElements % 5 ==0):
 if (PElements % 5 ==0):
    Plines = int(PElements/5)
 
-print "MO Lines = ", MOlines, "\n"
-print "P Lines = ", Plines, "\n"
-
 MOrawa = np.zeros(MOElements)
 MOrawb = np.zeros(MOElements)
 TotalPraw1 = np.zeros(PElements) 
@@ -136,9 +137,7 @@ with open(filename1,'r') as origin:
         if  "Alpha MO coefficients" in line:              
               i=i+1
               AMO=i
-              print "Alpha MO coefficients starts at line :", i
               j=i+MOlines-1
-              print "Alpha MO coefficients ends at line :", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -150,9 +149,7 @@ with open(filename1,'r') as origin:
               r = 0
               i=i+1
               BMO = i
-              print "Beta MO coefficients starts at line :", i
               j=i+MOlines-1
-              print "Beta MO coefficients ends at line :", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -164,9 +161,7 @@ with open(filename1,'r') as origin:
               i=i+1
               r = 0
               p = 0
-              print "Total SCF Density starts at line :", i
               j=i+Plines-1
-              print "Total SCF Density ends at line :", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -178,9 +173,7 @@ with open(filename1,'r') as origin:
               i=i+1
               r = 0
               p = 0
-              print "Spin SCF Density starts at line: ", i
               j=i+Plines-1
-              print "Spin SCF Density ends at line: ", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -195,9 +188,7 @@ with open(filename2,'r') as origin:
               i=i+1
               r = 0
               p = 0
-              print "Total SCF Density starts at line :", i
               j=i+Plines-1
-              print "Total SCF Density ends at line :", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -209,9 +200,7 @@ with open(filename2,'r') as origin:
               i=i+1
               r = 0
               p = 0
-              print "Spin SCF Density starts at line: ", i
               j=i+Plines-1
-              print "Spin SCF Density ends at line: ", j
               for m in range(0,j-i+1):
                  nextline = origin.next()
                  nextline = nextline.split()
@@ -227,22 +216,17 @@ SpinPraw1 = np.around(SpinPraw1,decimals=acc)
 TotalPraw2 = np.around(TotalPraw2,decimals=acc)
 SpinPraw2 = np.around(SpinPraw2,decimals=acc)
 
-print "Stored Alpha MO Coefficients matrix = \n", MOrawa
-print "Stored Beta MO Coefficients matrix = \n", MOrawb
-print "Stored Total SCF Density matrix = \n", TotalPraw1
-print "Stored Spin SCF Density matrix = \n", SpinPraw1
-
 PalphaRaw1 = (np.add(TotalPraw1,SpinPraw1)) * 0.5
 PbetaRaw1 = (np.subtract(TotalPraw1,SpinPraw1)) * 0.5 
 
 PalphaRaw2 = (np.add(TotalPraw2,SpinPraw2)) * 0.5
 PbetaRaw2 = (np.subtract(TotalPraw2,SpinPraw2)) * 0.5
 
-print "Ground state: Alpha Density matrix =\n", PalphaRaw1
-print "Ground state: Beta Density matrix =\n", PbetaRaw1
+#print "Ground state: Alpha Density matrix =\n", PalphaRaw1
+#print "Ground state: Beta Density matrix =\n", PbetaRaw1
 
-print "Detached state: Alpha Density matrix =\n", PalphaRaw2
-print "Detached state: Beta Density matrix =\n", PbetaRaw2
+#print "Detached state: Alpha Density matrix =\n", PalphaRaw2
+#print "Detached state: Beta Density matrix =\n", PbetaRaw2
 
 #Part 2: The NIO Code
 
@@ -258,14 +242,17 @@ P2a = symmetrize(PalphaRaw2)
 P1b = symmetrize(PbetaRaw1)
 P2b = symmetrize(PbetaRaw2)
 
-print "Alpha Density 1 = \n", P1a
-print "Beta Density 1 = \n", P1b
-print "Alpha Density 2 = \n", P2a
-print "Beta Density 2 = \n", P2b
+#print "Alpha Density 1 = \n", P1a
+#print "Beta Density 1 = \n", P1b
+#print "Alpha Density 2 = \n", P2a
+#print "Beta Density 2 = \n", P2b
+
+Pa1, Pb1 = MatGrab(filename1,NBasis,2)
+Pa2, Pb2 = MatGrab(filename2,NBasis,2)
 
 #Difference Density Matrices
-Dpa = np.subtract(P2a,P1a)
-Dpb = np.subtract(P2b,P1b)
+Dpa = Pa2 - Pa1
+Dpb = Pb2 - Pb1 
 
 print "Alpha Difference Density = \n", Dpa
 print "Beta Difference Density = \n", Dpb
@@ -283,93 +270,96 @@ for i in range(0,NBasis):
 #C = np.transpose(C)
 #Cb = np.transpose(Cb)
 
-print "Alpha MO Coefficient Matrix = \n", C
-print "Beta MO Coefficient Matrix = \n", Cb
-
 #Inverse of C:
 CInv = np.linalg.inv(C)
 CbInv = np.linalg.inv(Cb)
 #Overlap Matrix:
 # S = (C**(-1)t*(C**(-1))
-S = np.dot(np.transpose(CInv),CInv)
-print "Overlap Matrix = \n", S
+S_old = np.dot(np.transpose(CInv),CInv)
 Sb = np.dot(np.transpose(CbInv),CbInv)
 #Checkpoint: <Dp.S> = -1
-print "Overlap Matrix (test) = \n", S
+
+C_test = MatGrab(filename1,NBasis,1)
+Cb_test = MatGrab(filename1,NBasis,-1)
+S = GetOverlap(C_test,NBasis)
+C_test = column2square(C_test,NBasis)
+Cb_test = column2square(Cb_test,NBasis)
+
 DpSa = np.dot(Dpa,S)
 DpSb = np.dot(Dpb,S)
 
 print "Alpha <DeltaP S> = ", np.trace(DpSa)
 print "Beta <DeltaP S> = ", np.trace(DpSb)
 
+C = C_test
+Cb = Cb_test
+
 #Calculate S**(0.5)
 Svals, Svecs = np.linalg.eig(S)
 Sval_minhalf = (np.diag(Svals**(0.5)))
 Shalf = np.dot(Svecs,np.dot(Sval_minhalf,np.transpose(Svecs)))
-print "S**(0.5) = \n", Shalf
+Shalf = Shalf.real
+
+S_try = np.dot(Shalf,Shalf)
+diff = S_try - S
 
 #Calculae S**(0.5).Dpa.S**(0.5) and S**(0.5).Dpb.S**(0.5)
 #Let these two matrices be Z1 and Z2, respectively
 
 Sleft, Sval, Sright = np.linalg.svd(S)
 
-print "Sleft =", Sleft
-print "Sright =", Sright
-print "Svals = ", Sval
-
-print "Diff Svals =", Svals -  Sval
-
 I = np.eye(NBasis)
 
 Sval_I = np.multiply(Sval,I)
-print "Identity = ", I
-print "Sval_I", Sval_I
 
 for i in range(0,NBasis):
     Sval_I[i,i] = Sval_I[i,i]**(0.5)
 
+
 S_test = np.dot(Sleft,np.dot(Sval_I,Sright))
-print "S test", S_test
 
 Shalf = S_test
 
 Z1 = np.dot(Shalf,np.dot(Dpa,Shalf))
 Z2 = np.dot(Shalf,np.dot(Dpb,Shalf))
 
-print "Z1 = \n", Z1
-print "Z2 = \n", Z2
 
 #Diagonaize Z1 and Z2, obtain eigenvalues (e1 and e2) and eigenvectors (U1 and U2)
 
-e1, U1 = np.linalg.eig(Z1)
-e2, U2 = np.linalg.eig(Z2)
+e1, U1 = np.linalg.eig(np.dot(Shalf,np.dot(Dpa,Shalf)))
+e2, U2 = np.linalg.eigh(np.dot(Shalf,np.dot(Dpb,Shalf)))
 
-print "Alpha NIO Eigenvalues = \n", e1
-print "Beta NIO Eigenvalues = \n", e2
+e1 = e1.real
+e2 = e2.real
+U1 = U1.real
+U2 = U2.real
+
 print "Alpha NIO Eigenvectors = \n", U1
 print "Beta NIO Eigenvectors = \n", U2
+print "Alpha NIO Eigenvalues = \n", e1
+print "Beta NIO Eigenvalues = \n", e2
 
 #Backtransform U1 and U2 into the AO Basis
 
 V1 = np.dot(np.linalg.inv(Shalf),U1)
 V2 = np.dot(np.linalg.inv(Shalf),U2)
 
-print "V1 =\n", V1, "\n"
-print "V2 =\n", V2, "\n" 
+V1 = V1.real
+V2 = V2.real
+
+print "Alpha NIO Coefficients =\n", V1, "\n"
+print "Beta NIO Coefficients =\n", V2, "\n" 
 
 ########## HH +
 
 # Screen the NIO eigenvalues, pick up the ones we need.
 
-print "Snippet test: Starting..\n"
 
 # Calculate T=CTranspose.S.U
 
 Ta = np.dot(np.transpose(C),np.dot(S,V1))
 Tb = np.dot(np.transpose(Cb),np.dot(S,V2))
 
-print "Ta =", Ta, "\n"
-print "Tb =", Tb, "\n"
 
 for i in range(0,NBasis):
     if (np.absolute(e1[i].real) >= minEigVal):
@@ -426,18 +416,11 @@ for i in range(0,NBasis):
 
 pointer=0
 counter=1
-print "AMO = ", AMO
-print "BMO = ", BMO
 with open(filename1,'r') as origin:
   data = origin.readlines()
-  print "Line 5 = ", data[5]
-  print "Line 6 = ", data[6]
-  print "length of data = ", len(data)
   if "Alpha Orbital Energies" in line:
     AOE = i  
     BOE = AOE + int(NBasis/5) + 1
-    print "AOE line is: ", data[AOE]
-    print "BOE line is: ", data[BOE]
   with open(filename3,'w') as f2:
 
       print "Writing results to new output file: ", filename3, " ... "
@@ -459,7 +442,7 @@ with open(filename1,'r') as origin:
       counter =1      
 #Write the Beta eigenvalues, overwrite the Beta Orbital energies
       BOE = AOE + (int(NBasis/5)+2)
-      print "BOE at line =", BOE
+ #     print "BOE at line =", BOE
       if (NBasis%5 != 0):
           f2.write("\n")
       if (NBasis%5 == 0):
@@ -517,13 +500,12 @@ with open(filename1,'r') as origin:
       counter = 1
       if (NBasis%5 != 0):
          f2.write("\n")
-      print "NBasis element = ", V2[0,NBasis-1]
 #Copy the rest of the checkpoint file (unchanged)      
       pointer = BMO + (int(NBasis*NBasis/5))+2
       while (pointer < len(data)):
          f2.write(data[pointer])
          pointer = pointer+1
 print "NBasis = ", NBasis
-print "length of pa =",  len(P1a), "\n"
 print "Copying data to new checkpoint file: DONE"
 print "NIO Calculation done!"
+
